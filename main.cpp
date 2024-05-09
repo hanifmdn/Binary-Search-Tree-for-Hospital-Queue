c95ba8ddaa7907b2bd14f0af502470e8d797bfd2
DAliRIJTK <148074211+DAliRIJTK@users.noreply.github.com>
2024-05-09 15:22:44 +0700

    Header dan Main
    Mengubah looping while menjadi do while. memasukkan nama nama modul di body ke header

--- main.cpp
+++ main.cpp
@@ -1,57 +1,61 @@
 #include <iostream>
 #include <stdio.h>
-#include "GAANTRE.h"
+#include "gaantre.h"
 
 int main() {
-	int opsi = 1;
-	int display = 1;
+	int opsi ;
+	char pilihan;
 	
-	while (opsi != 0) {
-		switch (display) {
-			case 1:
-				printf("|+|======================================================================================|+|\n");
-                printf("|+|                                                                                      |+|\n");
-                printf("|+|                                Selamat Datang di GaAntre                             |+|\n");
-                printf("|+|                             Kamu Ga Perlu CAPE-CAPE Ngantre                          |+|\n");
-                printf("|+|                                                                                      |+|\n");
-                printf("|+|======================================================================================|+|\n");
-			case 2:
-				printf("|+|======================================================================================|+|\n");
-				printf("|+|                                                                                      |+|\n");
-				printf("|+|                                         GaAntre                                      |+|\n");
-				printf("|+|                             Kamu Ga Perlu CAPE-CAPE Ngantre                          |+|\n");
-				printf("|+|                                                                                      |+|\n");
-				printf("|+|======================================================================================|+|\n");
-				printf("|+|                                                                                      |+|\n");
-				printf("|+|                                         LOGIN                                        |+|\n");
-				printf("|+|                                                                                      |+|\n");
-				printf("|+|======================================================================================|+|\n");
+	do {
+		printf("|+|======================================================================================|+|\n");
+        printf("|+|                                                                                      |+|\n");
+        printf("|+|                                Selamat Datang di GaAntre                             |+|\n");
+        printf("|+|                             Kamu Ga Perlu CAPE-CAPE Ngantre                          |+|\n");
+        printf("|+|                                                                                      |+|\n");
+        printf("|+|======================================================================================|+|\n");
+        printf("|+|Lanjut (y/n)? ");
+		scanf(" %c", &pilihan);
+		
+		switch (pilihan) {
+			case 'y' :
+				system("cls");
+				menu();
 				printf("|+|Sudah punya akun?                                                                     |+|\n");
 				printf("|+|1. Sudah                                                                              |+|\n");
 				printf("|+|2. Belum                                                                              |+|\n");
 				printf("|+|0. Keluar                                                                             |+|\n");
-				printf("|+|Pilih opsi:                                                                           |+|\n");
-				printf("|+|======================================================================================|+|\n");
-				scanf("%d", &opsi);
-				if (opsi == 1) {
-					opsi = 3;
-				} else 
-				if (opsi == 2) {
-					opsi = 2;
+				printf("|+|Pilih opsi: ");
+	        	scanf("%d", &opsi);
+	        	
+	        	switch (opsi){
+		        	case 1:
+						system("cls");
+						menu();
+						login();
+				case 2:
+					system(cls);
+					menu();
+					membuatAkun();
+				//case 3:
+					// belum punya
+				//case 4:
+					// login
+				//case 5:
+					// tampilan admin
+				//case 6:
+					// tampilan pasien
+				default:
+					printf("Pilih opsi yang sesuai!");
 				}
+				
+	        case 'n' :
+	        	break;
+	        	
 
-			case 3:
-				// belum punya
-			case 4:
-				// login
-			case 5:
-				// tampilan admin
-			case 6:
-				// tampilan pasien
 			default:
 				printf("Pilih opsi yang sesuai!");
 		}
-	}
+	} while (opsi != 0);
 	
 
 
