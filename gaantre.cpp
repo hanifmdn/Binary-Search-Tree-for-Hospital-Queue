67f040a7fe2ea2d049fdd4ae4f372741ace6db27
Hanif Ahmad Naufal <153438391+ramenyeon@users.noreply.github.com>
2024-05-09 17:23:56 +0700

    Add files via upload

--- gaantre.cpp
+++ gaantre.cpp
@@ -1,51 +1,79 @@
 #include "GAANTRE.h"
 
-
-void membuatAkun () {
+void membuatAkun (int *display) {
+	char username[20];
 	FILE *akunPengguna;
+	bool ditemukan = false;
 	account akun;
-	printf("|+|                                                                                      |+|\n");
-	printf("|+|                                      Buat Akun                                       |+|\n");
-	printf("|+|                                                                                      |+|\n");
-	printf("|+|======================================================================================|+|\n");
-	printf("|+|Masukan username: ");
+	
+	printf("|+|Username: "); 
 	scanf(" %[^\n]", akun.username);
-	printf("|+|Masukan password: ");
+	printf("|+|Password: ");
 	scanf(" %[^\n]", akun.password);
 	
 	akunPengguna = fopen("akunPengguna.txt", "a+");
 	
-	if (akunPengguna != NULL) {
-		while (!EOF) {
-			// cek akun sudah ada atau belum
-			// jika sudah ada kembali ke punya akun
+	if (akunPengguna == NULL) {
+		printf("Tidak dapat membuka file akunPengguna.txt\n");
+	}
+	
+	while (fscanf(akunPengguna, "%s", username) == 1) {
+		if (strcmp(akun.username, username) == 0) {
+			printf("Username sudah ada!");
+			ditemukan = true;
+			*display = 3;
+			break;
 		}
 	}
-	if (akunPengguna != NULL) {
+		
+	if (!ditemukan) {
 		fprintf(akunPengguna, "%s %s %d\n", akun.username, akun.password, 0);
 		printf("Akun berhasil dibuat!");
+		*display = 4;
 		fclose(akunPengguna);
-	} else {
-		printf("Tidak dapat membuka file Akun Pengguna\n");
 	}
 }
 
-void punyaAkun(){
-	
-}
-
-
-void login () {
+void login (int *display) {
+	FILE *akunPengguna;
 	account masuk;
-	printf("|+|                                                                                      |+|\n");
-	printf("|+|                                         LOGIN                                        |+|\n");
-	printf("|+|                                                                                      |+|\n");
-	printf("|+|======================================================================================|+|\n");
-	printf("Masukan username: ");
+	account cek[100];
+	int pengguna = 0;
+	bool ditemukan = false;
+	
+	akunPengguna = fopen("akunPengguna.txt", "r");
+	
+	if (akunPengguna == NULL) {
+		printf("Tidak dapat membuka file akunPengguna.txt");
+	}
+	printf("|+|Username: "); 
 	scanf(" %[^\n]", masuk.username);
-	printf("Masukan password: ");
+	printf("|+|Password: ");
 	scanf(" %[^\n]", masuk.password);
 	
+	while (fscanf(akunPengguna, "%s %s %d", &cek[pengguna].username, &cek[pengguna].password, &cek[pengguna].identifier) == 3) {
+		if (strcmp(cek[pengguna].username, masuk.username) == 0) {
+			ditemukan = true;
+			break;
+		}
+		pengguna++;
+	}
+	if (ditemukan) {
+		if (strcmp(cek[pengguna].password, masuk.password) == 0) {
+			printf("berhasil masuk!");
+			if (cek[pengguna].identifier == 1) {
+				*display = 5;
+			} else {
+				*display = 6;
+			}
+		} else {
+			printf("Password Salah!");
+			
+		}
+		
+	} else {
+		printf("Username tidak ada! Silahkan buat akun terlebih dahulu!");
+	}
 	// buka file
 	// cek satu satu
 	// kalo username ada, nama ada berarti bisa masuk
@@ -60,7 +88,6 @@ Tree createNode () {
 void cekKosong () {
 	
 }
-
 
 Tree push () {
 	
@@ -92,5 +119,3 @@ void daftarPengguna () {
 void antrianSekarang () {
 	// menampilkan antrian sekarang (root)
 }
-
-
--- main.cpp
+++ main.cpp
@@ -1,80 +1,100 @@
 #include <iostream>
 #include <stdio.h>
-#include "gaantre.h"
+#include "GAANTRE.h"
 
 int main() {
-	int opsi ;
-	char pilihan;
 	
-	do {
-		printf("|+|======================================================================================|+|\n");
-        printf("|+|                                                                                      |+|\n");
-        printf("|+|                                Selamat Datang di GaAntre                             |+|\n");
-        printf("|+|                             Kamu Ga Perlu CAPE-CAPE Ngantre                          |+|\n");
-        printf("|+|                                                                                      |+|\n");
-        printf("|+|======================================================================================|+|\n");
-        printf("|+|Lanjut (y/n)? ");
-		scanf(" %c", &pilihan);
-		
-		switch (pilihan) {
-			case 'y' :
-				//system("cls");
+	int opsi = 1;
+	char opsichar;
+	int display = 1;
+	
+	while (opsi != 0) {
+		switch (display) {
+			case 1:
+				printf("|+|======================================================================================|+|\n");
+                printf("|+|                                                                                      |+|\n");
+                printf("|+|                                Selamat Datang di GaAntre                             |+|\n");
+                printf("|+|                             Kamu Ga Perlu CAPE-CAPE Ngantre                          |+|\n");
+                printf("|+|                                                                                      |+|\n");
+                printf("|+|--------------------------------------------------------------------------------------|+|\n");
+                printf("|+|Lanjut? (y/n) ");
+                scanf(" %c", &opsichar);
+                if (opsichar == 'y') {
+                	display = 2;
+				} else
+				if (opsichar == 'n') {
+					opsi = 0;
+				}
+			case 2:
+				system("cls");
 				printf("|+|======================================================================================|+|\n");
 				printf("|+|                                                                                      |+|\n");
 				printf("|+|                                         GaAntre                                      |+|\n");
-				printf("|+|                                Ga Perlu CAPE-CAPE Ngantre                            |+|\n");
+				printf("|+|                             Kamu Ga Perlu CAPE-CAPE Ngantre                          |+|\n");
+				printf("|+|                                                                                      |+|\n");
+				printf("|+|======================================================================================|+|\n");
+				printf("|+|                                                                                      |+|\n");
+				printf("|+|                                         Sign Up                                      |+|\n");
 				printf("|+|                                                                                      |+|\n");
 				printf("|+|======================================================================================|+|\n");
 				printf("|+|Sudah punya akun?                                                                     |+|\n");
 				printf("|+|1. Sudah                                                                              |+|\n");
 				printf("|+|2. Belum                                                                              |+|\n");
 				printf("|+|0. Keluar                                                                             |+|\n");
+				printf("|+|--------------------------------------------------------------------------------------|+|\n");
 				printf("|+|Pilih opsi: ");
-	        	scanf("%d", &opsi);
-	        	
-	        	switch (opsi){
-		        	case 1:
-						//system("cls");
-						printf("|+|======================================================================================|+|\n");
-						printf("|+|                                                                                      |+|\n");
-						printf("|+|                                         GaAntre                                      |+|\n");
-						printf("|+|                                Ga Perlu CAPE-CAPE Ngantre                            |+|\n");
-						printf("|+|                                                                                      |+|\n");
-						printf("|+|======================================================================================|+|\n");
-						login();
-					case 2:
-						//system("cls");
-						printf("|+|======================================================================================|+|\n");
-						printf("|+|                                                                                      |+|\n");
-						printf("|+|                                         GaAntre                                      |+|\n");
-						printf("|+|                                Ga Perlu CAPE-CAPE Ngantre                            |+|\n");
-						printf("|+|                                                                                      |+|\n");
-						printf("|+|======================================================================================|+|\n");
-						membuatAkun();
-					//case 3:
-						// belum punya
-					//case 4:
-						// login
-					//case 5:
-						// tampilan admin
-					//case 6:
-						// tampilan pasien
-					default:
-						printf("Pilih opsi yang sesuai!");
-					}
+				scanf("%d", &opsi);
+				if (opsi == 1) {
+					display = 4;
+				} else 
+				if (opsi == 2) {
+					display = 3;
+				}
+				break;
 
-	        case 'n' :
-	        	printf("|+|======================================================================================|+|\n");
-	        	printf("|+|                                                                                      |+|\n");
-	        	printf("|+|                        Terima Kasih Telah menggunakan GaAntre                        |+|\n");
-	        	printf("|+|                                                                                      |+|\n");
-	        	printf("|+|======================================================================================|+|\n");
-	        	break;
-	        	
+			case 3:
+				printf("|+|======================================================================================|+|\n");
+				printf("|+|                                                                                      |+|\n");
+				printf("|+|                                       GaAntre                                        |+|\n");
+				printf("|+|                             Kamu Ga Perlu CAPE-CAPE Ngantre                          |+|\n");
+				printf("|+|                                                                                      |+|\n");
+				printf("|+|======================================================================================|+|\n");
+				printf("|+|                                                                                      |+|\n");
+				printf("|+|                                      Buat Akun                                       |+|\n");
+				printf("|+|                                                                                      |+|\n");
+				printf("|+|======================================================================================|+|\n");
+				printf("|+|Silahkan buat akun baru                                                               |+|\n");
+				membuatAkun(&display);
+				break;
+				
+
+			case 4:
+				printf("|+|======================================================================================|+|\n");
+				printf("|+|                                                                                      |+|\n");
+				printf("|+|                                        GaAntre                                       |+|\n");
+				printf("|+|                                Ga Perlu CAPE-CAPE Ngantre                            |+|\n");
+				printf("|+|                                                                                      |+|\n");
+				printf("|+|======================================================================================|+|\n");                                                                                                                                                                   
+				printf("|+|                                                                                      |+|\n");
+				printf("|+|                                         LOGIN                                        |+|\n");
+				printf("|+|                                                                                      |+|\n");
+				printf("|+|======================================================================================|+|\n");
+				printf("|+|Silahkan Login                                                                        |+|\n");
+				login(&display);
+				break;
+	
+			case 5:
+				printf("masuk tampilan admin");
+				opsi = 0;
+				break;
+			case 6:
+				printf("masuk tampilan pasien");
+				opsi = 0;
+				break;
 			default:
 				printf("Pilih opsi yang sesuai!");
 		}
-	} while (opsi != 0);
+	}
 	
 
 
